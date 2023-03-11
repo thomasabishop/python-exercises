6474dd4ff17a9d3ccc154380bd51ce27e7c2a3d5
thomasabishop <tactonbishop@gmail.com>
2023-03-11 15:00:56 +0000

    complete 2022-day-one-part-two

--- 2022/01/B/solution.py
+++ 2022/01/B/solution.py
@@ -2,19 +2,20 @@ test_input = "/home/thomas/repos/advent_of_python/2022/01/input.test.txt"
 main_input = "/home/thomas/repos/advent_of_python/2022/01/input.txt"
 
 with open(main_input, "r") as f:
-    greatest_total = 0
     running_total = 0
-
+    totals = []
     lines = f.readlines()
     lines = [line.strip() for line in lines]
-
+    lines.append('')  # super hacky!
     i = 0
     for i in range(i, len(lines)):
         if lines[i] == '':
-            if running_total > greatest_total:
-                greatest_total = running_total
+            totals.append(running_total)
             running_total = 0
             continue
         else:
             running_total += int(lines[i])
-    print(greatest_total)
+    totals.sort(reverse=True)
+    highest_three = sum([totals[0], totals[1], totals[2]])
+
+print(highest_three)
