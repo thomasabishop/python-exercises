19164852142f6a0d86b58f9a46a511ce18a28e01
thomasabishop <tactonbishop@gmail.com>
2023-08-20 19:09:13 +0100

    test: add test for divisible numbers exercies

--- other-exercises/divisible_numbers/divisible_numbers.py
+++ other-exercises/divisible_numbers/divisible_numbers.py
@@ -1,14 +1,14 @@
-'''
+"""
 Write a program which will find all such numbers which are divisible by
 7 but are not a multiple of 5, between 2000 and 3200 (both included).
 The numbers obtained should be printed in a comma-separated sequence on
 a single line.
-'''
+"""
 
 
-def identify_matches():
+def identify_matches(lower, upper):
     matches = set()
-    for i in range(200, 3200):
+    for i in range(lower, upper):
         if i % 7 == 0 and i % 5 != 0:
             matches.add(i)
     convert_to_string = [str(num) for num in matches]
@@ -18,8 +18,8 @@ def identify_matches():
 
 
 def main():
-    print(identify_matches())
+    print(identify_matches(200, 3200))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ other-exercises/divisible_numbers/divisible_numbers_test.py
@@ -0,0 +1,5 @@
+import divisible_numbers
+
+
+def test_divisible_numers():
+    assert divisible_numbers.identify_matches(1, 15) == "14, 7"
