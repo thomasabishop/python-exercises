e27dbe5c8da63a2d4c492d2524259858b531ba54
thomasabishop <tactonbishop@gmail.com>
2023-12-11 18:18:48 +0000

    chore: set-up 2023 directory

---
+++ advent_of_code/2023/01/solution.py
@@ -0,0 +1,16 @@
+test_input = (
+    "/home/thomas/repos/python_exercises/advent_of_code/2023/data/test_input.txt"
+)
+
+
+def getInput(file_path):
+    with open(file_path, "r") as file:
+        return [line.strip() for line in file]
+
+
+test_input = getInput(test_input)
+
+
+def solution(input):
+    return input
+    # print(input)
