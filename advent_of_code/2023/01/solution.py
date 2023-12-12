2b93e799f834e18840172565674fa4c1fc4e8ab1
thomasabishop <thomas.actonbishop@bbc.co.uk>
2023-12-12 12:41:42 +0000

    exercise: complete AOC 23 1.A

--- advent_of_code/2023/01/solution.py
+++ advent_of_code/2023/01/solution.py
@@ -1,6 +1,7 @@
 import os
 
-puzzle_input_path = os.path.join(os.path.dirname(__file__), "data/test_input.txt")
+puzzle_input_path = os.path.join(
+    os.path.dirname(__file__), "data/test_input.txt")
 
 
 def getPuzzleInput(file_path):
@@ -12,7 +13,12 @@ puzzle_input = getPuzzleInput(puzzle_input_path)
 
 
 def solution(puzzle_input):
-    print(puzzle_input)
+    count = 0
+    for item in puzzle_input:
+        characters = list(item)
+        integers = [i for i in characters if i.isnumeric()]
+        count += int(integers[0] + integers[-1])
+    return count
 
 
-solution(puzzle_input)
+print(solution(puzzle_input))
