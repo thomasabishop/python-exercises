f62f230c318ac45e2abdcc78b280fc16a38ac537
thomasabishop <thomas.actonbishop@bbc.co.uk>
2023-12-18 20:37:30 +0000

    birmingham work

--- advent_of_code/2023/01/test_solution.py
+++ advent_of_code/2023/01/test_solution.py
@@ -1,7 +1,19 @@
-from solution import solution
-
-mock_input = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+from solution import solution_one, solution_two
 
 
-def test_solution():
-    assert solution(mock_input) == 142
+def test_solution_one():
+    mock_input = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+    assert solution_one(mock_input) == 142
+
+
+def test_solution_two():
+    mock_input = [
+        "two1nine",
+        "eightwothree"
+        "abcone2threexyz",
+        "xtwone3four",
+        "4nineeightseven2",
+        "zoneight234",
+        "7pqrstsixteen"
+    ]
+    assert solution_two(mock_input) == 281
