f5a5f33d4be332cf44354d4fba149c1d01c9f7b4
thomasabishop <tactonbishop@gmail.com>
2024-01-04 17:09:39 +0000

    chore: refactor file reading

--- advent_of_code/2023/02/solution_02_B.py
+++ advent_of_code/2023/02/solution_02_B.py
@@ -1,25 +1,26 @@
 import os
-from helpers import getPuzzleInput
 
 puzzle_input_path = os.path.join(os.path.dirname(__file__), "data/input.txt")
-puzzle_input = getPuzzleInput(puzzle_input_path)
-
 
 with open(puzzle_input_path, "r") as f:
     total = 0
-    for line in puzzle_input:
+    lines = [line.strip() for line in f.readlines()]
+
+    for line in lines:
         colour_sets = {"red": [], "green": [], "blue": []}
         colon_separator = line.index(":")
         games = line[colon_separator + 1 :].split(";")
         games = [item.strip().split() for game in games for item in game.split(",")]
-        print(games)
+
         for counter_values in games:
             colour = counter_values[1]
             count = int(counter_values[0])
             colour_sets[colour].append(count)
         reduced = [max(colour_sets[colour]) for colour in colour_sets]
         reduced_product = 1
+
         for val in reduced:
             reduced_product *= val
+
         total += reduced_product
     print(total)
